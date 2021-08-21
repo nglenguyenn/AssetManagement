422c94895dd65b486bf21638e1d002d5358a004b
Nguyen Le Nguyen <nglenguyenn@gmail.com>
2021-08-21 15:26:25 +0700

    Delete AdminPW and Location

--- Rookie.AssetManagement.DataAccessor/Entities/AdminPassword.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rookie.AssetManagement.DataAccessor.Entities
-{
-    public static class AdminPassword
-    {
-        public static string AdminHCM = "123456";
-        public static string AdminHN = "123456";
-    }
-}
